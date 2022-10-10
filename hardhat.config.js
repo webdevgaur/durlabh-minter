d24e7c99d8c9c046b5f289d00495ab5758b71638
Anurag Gaur <anuraggaur495@gmail.com>
2022-10-10 07:16:42 +0530

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,9 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.4",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+};
