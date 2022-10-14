1d054353ee6675ed763b8d012d74cd86fdb20386
Anurag Gaur <anuraggaur495@gmail.com>
2022-10-15 00:42:06 +0530

    UI finalised

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,12 @@ function App() {
   const [accounts, setAccounts] = useState([]);
 
   return (
-    <div className="overlay">
+    <>
       <div className="App">
         <NavBar accounts={accounts} setAccounts={setAccounts} />
         <MainMint accounts={accounts} setAccounts={setAccounts} />
       </div>
-      <div className="moving-background">
-        
-      </div>
-    </div>
+    </>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Button, Flex, Image, Link, Spacer } from "@chakra-ui/react"
+import { Box, Button, Flex, Link, Spacer } from "@chakra-ui/react"
 import { AiFillTwitterCircle, AiFillYoutube, AiFillLinkedin, AiFillMail } from "react-icons/ai";
 
 const NavBar = ({ accounts, setAccounts }) => {
@@ -16,28 +16,56 @@ const NavBar = ({ accounts, setAccounts }) => {
         }
     };
 
+    const fakeClickHandler = () => {
+        alert('This is just a fake link for now. Sorry :(');
+    } 
+
     return(
         <Flex justify="space-between" align="center" padding="30px">
             
             {/* Left side of the navbar */}
-            <Link href="https://twitter.com/webdevgaur" target="_blank" rel="noopener noreferrer">
-                <AiFillTwitterCircle className="link-icon twitter" />
-            </Link>
-            <div>YouTube</div>
-            <div>LinkedIn</div>
-            <div>Email</div>
+            <Flex justify="space-around" width="40%" padding="0 75px">
+                <Link href="https://twitter.com/webdevgaur" target="_blank" rel="noopener noreferrer">
+                    <AiFillTwitterCircle className="link-icon twitter" />
+                </Link>
+                <Link href="https://www.youtube.com/c/anurag2402" target="_blank" rel="noopener noreferrer">
+                    <AiFillYoutube className="link-icon youtube" />
+                </Link>
+                <Link href="https://www.linkedin.com/in/webdevgaur/" target="_blank" rel="noopener noreferrer">
+                    <AiFillLinkedin className="link-icon linkedin" />
+                </Link>
+                <Link href="mailto:anuraggaur495@gmail.com?subject = Hi Anurag" target="_blank" rel="noopener noreferrer">
+                    <AiFillMail className="link-icon email" />
+                </Link>
+            </Flex>
 
             {/* Right side of the navbar */}
-            <div>About</div>
-            <div>Mint</div>
-            <div>Team</div>
+            <Flex justify="space-around" width="40%" padding="0 30px" align="center">
+                <Box margin="0 15px"><a  onClick={fakeClickHandler} href="#">About</a></Box>
+                <Spacer />
+                <Box margin="0 15px"><a  onClick={fakeClickHandler} href="#">Mint</a></Box>
+                <Spacer />
+                <Box margin="0 15px"><a  onClick={fakeClickHandler} href="#">Team</a></Box>
+                <Spacer />
+                {/* Connect wallet */}
+                {isConnected ? (
+                    <Box margin="0 15px">Connected 👍</Box>
+                ) : (
+                    <Button
+                     onClick={connectAccount}
+                     backgroundColor="deepskyblue"
+                     borderRadius="5px"
+                     boxShadow="0px 2px 2px 1px #0F0F0F"
+                     color="white"
+                     cursor="pointer"
+                     fontFamily="inherit"
+                     padding="15px"
+                     margin="0 15px"
+                     >Connect 🤝</Button>
+                )}
+            </Flex>
 
-            {/* Connect wallet */}
-            {isConnected ? (
-                <p>Connected 👍</p>
-            ) : (
-                <button onClick={connectAccount}>Connect 🤝</button>
-            )}
+            
            
         </Flex>
     );
